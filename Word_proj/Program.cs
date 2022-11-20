80af83762873ba9bdcb463f1db545da503532432
cupert <76869929+cupert@users.noreply.github.com>
2022-11-20 18:12:20 +0100

    Projektdateien hinzufügen.

---
+++ Word_proj/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Word_proj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Words word = new Words();
+            string test = word.getallWords();
+        }
+    }
+}
---
+++ Word_proj/Words.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Word_proj
+{
+    public class Words
+    {
+        public Words()
+        {
+
+        }
+        public string getallWords()
+        {
+            string text = "";
+            string path = "C:\\Users\\Cuper\\Desktop\\Cupi\\FH\\5.Sem\\fprog\\Wordcount\\Testfolder";
+            foreach (string file in Directory.EnumerateFiles(path, "*.txt", SearchOption.AllDirectories))
+            {
+                text += File.ReadAllText(file);
+                text += " ";
+
+            }
+            Console.WriteLine(text);
+            return text;
+        }
+    }
+}
